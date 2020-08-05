6a84f73ec712b0b11ac1e7fed0c70a2aa95f6610
Henri Susanto <henri@member.id>
2020-08-05 15:02:42 +0700

    prepare repository

--- src/modules/member/member.controller.ts
+++ src/modules/member/member.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { MemberService } from './member.service';
+import { MemberRepository } from './member.repository'
 
 @Controller('member')
 export class MemberController {
-  constructor(private readonly memberService: MemberService) {}
+    constructor(
+        private readonly memberService: MemberService,
+        private readonly memberRepository: MemberRepository
+    ) { }
 
-  @Get('hello')
-  getHello(): string {
-    return this.memberService.getHello();
-  }
+    @Get('hello')
+    getHello(): string {
+        return this.memberService.getHello();
+    }
 
+    @Get('henri')
+    sayHenri(): string {
+        return this.memberRepository.sayHenri()
+    }
 }
